264162b04aed4db5782d58978823f87124cdd36c
Nian Liu <nliu@bol.com>
2020-07-01 21:46:47 +0200

    random swing example

--- algorithms-java/src/main/java/com/github/nianliu/Hello.java
+++ algorithms-java/src/main/java/com/github/nianliu/Hello.java
@@ -1,8 +1,25 @@
 package com.github.nianliu;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new JFrame();
+        frame.setSize(300, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JComponent component = new JComponent() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                g.drawLine(20, 20, 100, 100);
+                g.drawOval(50,50, 3,3 );
+            }
+        };
+
+        frame.add(component);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
